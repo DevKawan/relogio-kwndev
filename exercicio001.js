25a3ed1a9fd083c87f96db4af8b1bf5627fd91ed
Kawan Franklin <80866461+DevKawan@users.noreply.github.com>
2023-02-07 08:50:00 -0300

    arrumando o código:
    Não estava mudando o background e as fotos. arrumado, colocando eles no msm escopo da const relogio

--- exercicio001.js
+++ exercicio001.js
@@ -24,26 +24,28 @@ const relogio = setInterval(function tempo() {
     horas.textContent = `${hr} :`
     minutos.textContent = `${min} :`
     segundos.textContent = `${s}`
+
+
+    // IF DAS MENSAGENS E BACKGROUND
+    if (hr >= 0 && hr < 12){
+        // BOM DIA
+        img.src = 'images/foto-manhã.jpg'
+        document.body.style.background = "#0033cc"
+        section.style.background = "#b38f00"
+        relogioBck.style.background = "#0033cc"
+    
+    } else if (hr >= 13 && hr <= 18){
+        // BOA TARDE
+        img.src = 'images/foto-tarde.jpg'
+        document.body.style.background = "#172081"
+        section.style.background = "#cc6600"
+        relogioBck.style.background = "#172081"
+    
+    } else {
+        // BOA NOITE
+        img.src = 'images/foto-noite.jpg'
+        document.body.style.background = "#18181a"
+        section.style.background = "#321ea1"
+        relogioBck.style.background = "#18181a"
+    }
 })
-
-// IF DAS MENSAGENS E BACKGROUND
-
-if (horas >= 0 && hora < 12){
-    // BOM DIA
-    img.src = 'images/foto-manhã.jpg'
-    document.body.style.background = "#0033cc"
-    section.style.background = "#b38f00"
-    relogioBck.style.background = "#0033cc"
-} else if (horas >= 13 && hora <= 18){
-    // BOA TARDE
-    img.src = 'images/foto-tarde.jpg'
-    document.body.style.background = "#172081"
-    section.style.background = "#cc6600"
-    relogioBck.style.background = "#172081"
-} else {
-    // BOA NOITE
-    img.src = 'images/foto-noite.jpg'
-    document.body.style.background = "#18181a"
-    section.style.background = "#321ea1"
-    relogioBck.style.background = "#18181a"
-}
